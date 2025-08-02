97ace69e27598649b8333acf158ff3396ed9e5d7
Awelani Murovhi <awelani@leaseup.co.za>
2025-08-03 00:24:05 +0200

    fix(prisma): update rootEnvPath in generated client configuration to point to ../../.env

--- packages/prisma/generated/client/index.js
+++ packages/prisma/generated/client/index.js
@@ -482,7 +482,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../.env",
     "schemaEnvPath": "../../.env"
   },
   "relativePath": "../..",
