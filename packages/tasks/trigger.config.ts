5620a555a0be65ea5295a0d6bf9bc03779a0b056
Awelani Murovhi <awelani@leaseup.co.za>
2025-09-18 19:33:08 +0200

    chore(prisma): update Prisma client version to 6.5.0 in trigger configuration

--- packages/tasks/trigger.config.ts
+++ packages/tasks/trigger.config.ts
@@ -1,5 +1,9 @@
 import { defineConfig } from '@trigger.dev/sdk/v3';
 import { prismaExtension } from '@trigger.dev/build/extensions/prisma';
+import {
+  syncVercelEnvVars,
+  additionalFiles,
+} from '@trigger.dev/build/extensions/core';
 
 export default defineConfig({
   project: 'proj_jlhisfoufrjnyuppyumz',
@@ -10,8 +14,19 @@ export default defineConfig({
       prismaExtension({
         schema: '../prisma/schema.prisma',
         clientGenerator: 'client',
+        version: '6.9.0',
+        migrate: false,
+      }),
+      additionalFiles({
+        files: ['../prisma/generated/client/*'],
+      }),
+      syncVercelEnvVars({
+        vercelAccessToken: process.env.VERCEL_ACCESS_TOKEN,
+        projectId: process.env.VERCEL_PROJECT_ID,
+        vercelTeamId: process.env.VERCEL_TEAM_ID,
       }),
     ],
+    external: ['@prisma/client'],
   },
   maxDuration: 3600,
   retries: {
