e49cb9e335e21a9ff6d61a33aef61e858c797334
Awelani Murovhi <awelani@leaseup.co.za>
2025-10-13 11:31:21 +0200

    refactor(api): enhance error handling in tRPC route to redirect unauthorized requests and improve logging in development

--- apps/app/src/app/api/trpc/[trpc]/route.ts
+++ apps/app/src/app/api/trpc/[trpc]/route.ts
@@ -1,13 +1,9 @@
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
-import { type NextRequest } from "next/server";
+import { NextResponse, type NextRequest } from "next/server";
 
 import { appRouter } from "@leaseup/trpc/server";
 import { createTRPCContext } from "@leaseup/trpc/trpc";
 
-/**
- * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
- * handling a HTTP request (e.g. when you make requests from Client Components).
- */
 const createContext = async (req: NextRequest) => {
   return createTRPCContext({
     headers: req.headers,
@@ -20,14 +16,16 @@ const handler = (req: NextRequest) =>
     req,
     router: appRouter,
     createContext: () => createContext(req),
-    onError:
-      process.env.NODE_ENV === "development"
-        ? ({ path, error }) => {
-            console.error(
-              `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
-            );
-          }
-        : undefined,
+    onError: ({ error, path }) => {
+      if (error.code === "UNAUTHORIZED") {
+        return NextResponse.redirect(new URL("/sign-in", req.url));
+      }
+      if (process.env.NODE_ENV === "development") {
+        console.error(
+          `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
+        );
+      }
+    },
   });
 
 export { handler as GET, handler as POST };
